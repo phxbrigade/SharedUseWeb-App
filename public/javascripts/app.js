cec0360b92030712d988ad49e321d497109632a5
Ben Ebel <bge1234@gmail.com>
2017-06-26 21:00:16 -0700

    Add Angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,13 +2,41 @@ var app = angular.module('SharedUseWeb', ['ngRoute'])
 
 app.config(function ($routeProvider) {
   $routeProvider
-    .when('/login', {
-      templateUrl: 'partials/login.html',
-      controller: 'LoginController'
-    })
     .when('/', {
-      templateUrl: 'partials/main.html',
-      controller: 'MainController'
+      templateUrl: 'partials/main.html'
+    })
+    .when('/#', {
+      templateUrl: 'partials/main.html'
+    })
+    .when('/about', {
+      templateUrl: 'partials/aboutSharedUse.html'
+    })
+    .when('/codeforPHX', {
+      templateUrl: 'partials/codeforPHX.html'
+    })
+    .when('/contactt', {
+      templateUrl: 'partials/contact.html'
+    })
+    .when('/sites', {
+      templateUrl: 'partials/sites.html'
+    })
+    .when('/sites/cultivatesouthphx', {
+      templateUrl: 'partials/cultivatesouthphx.html'
+    })
+    .when('/sites/garfield', {
+      templateUrl: 'partials/garfield.html'
+    })
+    .when('/sites/kiser', {
+      templateUrl: 'partials/kiser.html'
+    })
+    .when('/sites/nadaburg', {
+      templateUrl: 'partials/nadaburg.html'
+    })
+    .when('/sites/solano', {
+      templateUrl: 'partials/solano.html'
+    })
+    .when('/sites/wilson', {
+      templateUrl: 'partials/wilson.html'
     })
     .otherwise({
       redirectTo: '/'
